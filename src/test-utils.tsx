b6bc2130e848c70cd1382358359ca1c38d34c631
SEO DONGHWI <sdh0279@gmail.com>
2025-06-08 21:45:56 +0900

    Add testing utilities and setup

---
+++ src/test-utils.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { render, RenderOptions } from '@testing-library/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { MemoryRouter } from 'react-router-dom'
+import { CompaniesProvider } from '@/contexts/CompaniesContext'
+
+const renderWithProviders = (
+  ui: React.ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>
+) => {
+  const queryClient = new QueryClient()
+
+  const Wrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => (
+    <QueryClientProvider client={queryClient}>
+      <CompaniesProvider>
+        <MemoryRouter>{children}</MemoryRouter>
+      </CompaniesProvider>
+    </QueryClientProvider>
+  )
+
+  return render(ui, { wrapper: Wrapper, ...options })
+}
+
+export * from '@testing-library/react'
+export { renderWithProviders }
+
